d87aac4b41ceba233095721e15f93f9c313fdbe3
thienit2003 <thien01226588737@gmail.com>
2024-11-07 20:39:12 +0700

    java baitap

---
+++ baitap.java
@@ -0,0 +1,159 @@
+import java.util.Scanner;
+
+public class baitap {
+    // public  class int1 {
+    //     public static void main(String[] args) {
+    //         int S=0;
+    //         Scanner scanner =  new Scanner(System.in);
+    //         int n= scanner .nextInt();
+    //         for (int i = 1; i <= n; i++) {
+    //             S=S+i;
+
+
+
+    //     }
+    //     System.out.printf("Tong S = %d\n",S);
+    //     scanner.close();
+    // }
+
+    // }
+    // public  class int2 {
+    //     public static void main(String[] args) {
+    //         int S=0;
+    //         Scanner scanner =  new Scanner(System.in);
+    //         int n= scanner .nextInt();
+    //         for (int i = 1; i <= n; i++) {
+    //             S=S+i*2;
+
+
+
+    //     }
+    //     System.out.printf("Tong S = %d\n",S);
+    //     scanner.close();
+    // }
+
+    // }
+//     
+// public class  int3 {
+//  public static void main(String[] args) {
+//     int S=0;
+//     Scanner scanner =  new Scanner(System.in);
+//     int n= scanner .nextInt();
+//     for (int i = 1; i <= n; i++) {
+//         S+=1/i;
+//  }
+//  System.out.printf("Tong S = %f\n",S);
+//  scanner.close();
+//  }
+    
+// }
+// public class  int4 
+// {
+//     public static void main(String[] args) 
+//     {
+//         double S=0.0;
+//         Scanner scanner =  new Scanner(System.in);
+//         int n= scanner .nextInt();
+//         for (int i = 1; i <= n; i++) 
+//         {
+//             S+=(double)1/(i*2);
+//         }
+//         System.out.printf("Tong S = %f\n",S);
+//         scanner.close();
+//     }
+// }
+
+// public class  int5 {
+// public static void main(String[] args) {
+//     double S=0.0;
+//     Scanner scanner = new Scanner(System.in);
+//     int n= scanner .nextInt();
+//     for(int i = 1; i<=n;i++)
+//     {
+//         S+=(double)1/(i*2 + 1);
+//     }
+//     System.out.printf("Tong S = %f\n",S);
+//     scanner.close();
+// }
+
+    
+// }
+// public  class int6 
+// {
+//     public static void main(String[] args) 
+//     {
+//         double S=0;
+//         Scanner scanner = new Scanner(System.in);
+//         int n= scanner .nextInt();
+//         for(int i = 1; i<=n;i++)
+//         {
+//             S+=(double)1/i*(i + 1);
+//         }
+//         System.out.printf("Tong S = %f\n",S);
+//         scanner.close();
+//     }
+// }
+// public class int7
+// {
+//     public static void main(String[] args) {
+//         double S=0.0;
+//         Scanner scanner = new Scanner(System.in);
+//         int  n= scanner .nextInt();
+//         scanner.close();
+//         for(int i = 1; i<=n;i++)
+//         {
+//             S+=(double) i/(i+1);
+//         }
+//         System.out.printf("Tong S = %f\n",S);
+
+//     }
+// }
+    // public class int8 {
+    //     public static void main(String[] args) 
+    //     {
+    //         double S=0.0;
+    //         Scanner scanner = new Scanner(System.in);
+    //         int n = scanner .nextInt();
+    //         scanner.close();
+    //         for(int i = 1; i<=n;i++)
+    //         {
+    //             S+=(double) (2*i+1)/(2*i+2);
+    //         }
+    //         System.out.printf("Tong S = %f\n",S);
+
+    //     }
+    
+        
+    // }
+    // public class int9
+    // {
+    //     public static void main(String[] args) {
+    //         int S=1;
+    //         Scanner scanner= new Scanner(System.in);
+    //         int n = scanner.nextInt();
+    //         scanner.close();
+    //         for(int i=1; i<=n;i++)
+    //         {
+    //             S=S*i;
+    //         }
+    //         System.out.printf("Tong S = %d\n",S);
+    //     }
+    // }
+    // public class int10
+    // {
+    //     public static void main(String[] args) {
+    //         double S=0.0;
+    //         System.err.println("nhap so n=");
+    //         Scanner scanner = new Scanner(System.in);
+    //         int n=scanner.nextInt();
+    //         System.out.println("nhap so x=");
+    //         int x= scanner.nextInt();
+    //         scanner.close();
+    //         S= Math.pow(n, x);
+    //         System.out.println("so S=",S);
+
+    //     }
+    // }
+
+
+}
